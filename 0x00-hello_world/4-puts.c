f66c9fd6f6882fe02599176d2085b9e95adb8475
Mahmoud <mahmoud35868220@gmail.com>
2023-08-14 04:04:40 -0700

    gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * Description: Prints a string to stdout.
+ * Return: 0 upon successful execution.
+ */
 int main(void)
-{ 
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+{
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
